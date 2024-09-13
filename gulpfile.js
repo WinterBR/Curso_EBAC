94a9bf5979f9058ddefee9f47f024c2156e81848
Eduardo Winter <eduardowinterbr@gmail.com>
2024-09-13 18:37:13 -0300

    img

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const obfuscate = require('gulp-obfuscate');
 const imagemin = require('gulp-imagemin')
 
 function comprimeIMG() {
-    return gulp.src('./source/images/*')
+    return gulp.src('./source/images/*', {encoding:false})
     .pipe(imagemin())
     .pipe(gulp.dest('./build/images'));
 }
