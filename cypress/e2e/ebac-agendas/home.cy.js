ddc5bcd7ea903dda1e0bea4e702b3f27512acb5f
Eduardo Winter <eduardowinterbr@gmail.com>
2024-11-11 19:01:18 -0300

    1

---
+++ cypress/e2e/ebac-agendas/home.cy.js
@@ -0,0 +1,29 @@
+describe('Agenda de Contatos', () => {
+    beforeEach(() => {
+        cy.visit('https://agenda-contatos-react.vercel.app/');
+    });
+  
+    it('Deve adicionar um novo contato', () => {
+        cy.get('[type="text"]').type('John Pork');
+        cy.get('[type="email"]').type('JohnPork@gmail.com');
+        cy.get('[type="tel"]').type('1234567890');
+        cy.get('.adicionar').click();
+        
+        cy.contains('John Pork').should('exist');
+        cy.contains('JohnPork@gmail.com').should('exist');
+        cy.contains('1234567890').should('exist');
+    });
+  
+    it('Deve editar um contato existente', () => {
+        cy.get(':nth-child(5) > .sc-gueYoa > .edit').click();
+        cy.get('[type="text"]').clear().type('Shaolin M. de Porco');
+        cy.get('[type="email"]').clear().type('ShaolinMatadorDePorco@gmail.com');
+        cy.get('[type="tel"]').clear().type('0987654321');
+        cy.get('.alterar').click();
+    });
+  
+    it('Deve remover um contato', () => {
+        cy.get(':nth-child(5) > .sc-gueYoa > .delete').click();
+    });
+  });
+  
